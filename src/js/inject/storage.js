c240981ee8cb07791fb37cbc55d8c210e1a5f9ad
adil <adil@brokergenius.com>
2018-06-29 20:03:32 +0530

    Getter setter method for store

--- src/js/inject/storage.js
+++ src/js/inject/storage.js
@@ -1,1 +1,13 @@
-const store  =  chrome.storage.sync;
+const store = chrome.storage.sync;
+
+export const setData = (data) => {
+    return new Promise(resolve => {
+        store.set({'currentQue': data}, (result) => resolve(result));
+    });
+};
+
+export const getData = () => {
+    return new Promise(resolve => {
+        store.get(['currentQue'], (data) => resolve(data))
+    });
+};
